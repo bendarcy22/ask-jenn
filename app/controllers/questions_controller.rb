af0a1b36e8a5acc3c6b7d3a1cfa07361ef6e0cc2
Padrig Tuck <Padrig@gmail.com>
2016-07-17 20:51:30 -0400

    fixed up errors

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,9 @@
+require 'twilio-ruby'
+
 class QuestionsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def index
     @questions = Question.all
   end
@@ -7,20 +12,27 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
-def new
-  @question = Question.new
-end
-
-def create
-  @question = Question.new(question_params)
-  if @question.save
-    flash[:notice] = "Jenn will get back to you shortly"
-    QuestionMailer.new_question(@question).deliver_later
-    redirect_to questions_path
-  else
-    render :new
+  def new
+    @question = Question.new
   end
-end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:notice] = "Jenn will get back to you shortly"
+      QuestionMailer.new_question(@question).deliver_later
+      notify(@question.name, @question.body)
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  def notify(from, msg)
+    client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+    message = client.messages.create from: '+16179367820', to: '+1', body: "#{from} - #{msg}"
+    render plain: message.status
+  end
 end
 
 private
